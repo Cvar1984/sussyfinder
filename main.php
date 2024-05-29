34c93254dd2748360c1b6bc50b79bd3a1da3baa1
Cvar1984 <cvar1984@cvar1984.my.id>
2024-05-29 11:38:10 +0700

    put file() to the last options

--- main.php
+++ main.php
@@ -227,21 +227,15 @@ function compareTokens($tokenNeedles, $tokenHaystack)
     }
     return $output;
 }
-
-function urlFileArray($url) {
-    if (function_exists('file')) {
-        $content = @file($url, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES); // Use error suppression for cleaner handling
-
-        // If file() fails, return false
-        if ($content === false) {
-            trigger_error("Failed to fetch URL using file", E_USER_WARNING);
-            return false;
-        }
-
-        return $content;
-    }
-
-    else if (function_exists('curl_init')) {
+/**
+ * Return array of string from url
+ *
+ * @param string $url
+ * @return array|false
+ */
+function urlFileArray($url)
+{
+    if (function_exists('curl_init')) {
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
@@ -259,9 +253,7 @@ function urlFileArray($url) {
         $content = curl_exec($ch);
         curl_close($ch);
         return explode("\n", $content);
-    }
-
-    else if (function_exists('file_get_contents')) {
+    } else if (function_exists('file_get_contents')) {
         $context = stream_context_create(
             array(
                 'http' => array(
@@ -283,6 +275,16 @@ function urlFileArray($url) {
         }
 
         return explode("\n", $content);
+    } else if (function_exists('file')) {
+        $content = @file($url, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES); // Use error suppression for cleaner handling
+
+        // If file() fails, return false
+        if ($content === false) {
+            trigger_error("Failed to fetch URL using file", E_USER_WARNING);
+            return false;
+        }
+
+        return $content;
     }
 
     trigger_error("No suitable methods found to fetch URL content", E_USER_WARNING);
@@ -431,138 +433,142 @@ $blacklistMD5Sums = urlFileArray('https://raw.githubusercontent.com/Cvar1984/sus
 ?>
 <!DOCTYPE html>
 <html lang="en-us">
+    <head>
+        <title>Sussy Finder</title>
+        <style type="text/css">
+            @import url('https://fonts.googleapis.com/css?family=Ubuntu+Mono&display=swap');
 
-<head>
-    <title>Sussy Finder</title>
-    <style type="text/css">
-        @import url('https://fonts.googleapis.com/css?family=Ubuntu+Mono&display=swap');
+            body {
+                font-family: 'Ubuntu Mono', monospace;
+                color: #8a8a8a;
+            }
 
-        body {
-            font-family: 'Ubuntu Mono', monospace;
-            color: #8a8a8a;
-        }
+            table {
+                border-spacing: 0;
+                padding: 10px;
+                border-radius: 7px;
+                border: 3px solid #d6d6d6;
+            }
 
-        table {
-            border-spacing: 0;
-            padding: 10px;
-            border-radius: 7px;
-            border: 3px solid #d6d6d6;
-        }
+            tr,
+            td {
+                padding: 7px;
+            }
 
-        tr,
-        td {
-            padding: 7px;
-        }
+            th {
+                color: #8a8a8a;
+                padding: 7px;
+                font-size: 25px;
+            }
 
-        th {
-            color: #8a8a8a;
-            padding: 7px;
-            font-size: 25px;
-        }
+            input[type=submit]:focus {
+                background: #ff9999;
+                color: #fff;
+                border: 3px solid #ff9999;
+            }
 
-        input[type=submit]:focus {
-            background: #ff9999;
-            color: #fff;
-            border: 3px solid #ff9999;
-        }
+            input[type=submit]:hover {
+                border: 3px solid #ff9999;
+                cursor: pointer;
+            }
 
-        input[type=submit]:hover {
-            border: 3px solid #ff9999;
-            cursor: pointer;
-        }
+            input[type=text]:hover {
+                border: 3px solid #ff9999;
+            }
 
-        input[type=text]:hover {
-            border: 3px solid #ff9999;
-        }
+            input {
+                font-family: 'Ubuntu Mono', monospace;
+            }
 
-        input {
-            font-family: 'Ubuntu Mono', monospace;
-        }
+            input[type=text] {
+                border: 3px solid #d6d6d6;
+                outline: none;
+                padding: 7px;
+                color: #8a8a8a;
+                width: 100%;
+                border-radius: 7px;
+            }
 
-        input[type=text] {
-            border: 3px solid #d6d6d6;
-            outline: none;
-            padding: 7px;
-            color: #8a8a8a;
-            width: 100%;
-            border-radius: 7px;
-        }
+            input[type=submit] {
+                color: #8a8a8a;
+                border: 3px solid #d6d6d6;
+                outline: none;
+                background: none;
+                padding: 7px;
+                width: 100%;
+                border-radius: 7px;
+            }
+        </style>
+    </head>
 
-        input[type=submit] {
-            color: #8a8a8a;
-            border: 3px solid #d6d6d6;
-            outline: none;
-            background: none;
-            padding: 7px;
-            width: 100%;
-            border-radius: 7px;
-        }
-    </style>
-</head>
-
-<body>
-    <script type="text/javascript">
-        function copytable(el) {
-            var urlField = document.getElementById(el)
-            var range = document.createRange()
-            range.selectNode(urlField)
-            window.getSelection().addRange(range)
-            document.execCommand('copy')
-        }
-    </script>
-    <form method="post">
-        <table align="center" width="30%">
-            <tr>
-                <th>
-                    Sussy Finder
-                </th>
-            </tr>
-            <tr>
-                <td>
-                    <input type="text" name="dir" value="<?= getcwd() ?>">
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <input type="submit" name="submit" value="SEARCH">
-                </td>
-            </tr>
-
-            <?php if (isset($_POST['submit'])) { ?>
+    <body>
+        <script type="text/javascript">
+            function copytable(el) {
+                var urlField = document.getElementById(el)
+                var range = document.createRange()
+                range.selectNode(urlField)
+                window.getSelection().addRange(range)
+                document.execCommand('copy')
+            }
+        </script>
+        <form method="post">
+            <table align="center" width="30%">
+                <tr>
+                    <th>
+                        Sussy Finder
+                    </th>
+                </tr>
                 <tr>
                     <td>
-                        <span style="font-weight:bold;font-size:25px;">RESULT</span>
-                        <input type=button value="Copy to Clipboard" onClick="copytable('result')">
+                        <input type="text" name="dir" value="<?= getcwd() ?>">
                     </td>
                 </tr>
-            </table>
-            <table id="result" align="center" width="30%">
-                <?php
-                $path = $_POST['dir'];
-                $result = getSortedByExtension($path, $ext);
+                <tr>
+                    <td>
+                        <input type="submit" name="submit" value="SEARCH">
+                    </td>
+                </tr>
 
-                $fileReadable = $result['file_readable'];
-                $fileNotWritable = $result['file_not_readable'];
-                $fileReadable = sortByLastModified($fileReadable);
+                <?php if (isset($_POST['submit'])) { ?>
+                    <tr>
+                        <td>
+                            <span style="font-weight:bold;font-size:25px;">RESULT</span>
+                            <input type=button value="Copy to Clipboard" onClick="copytable('result')">
+                        </td>
+                    </tr>
+                </table>
+                <table id="result" align="center" width="30%">
+                    <?php
+                    $path = $_POST['dir'];
+                    $result = getSortedByExtension($path, $ext);
 
-                foreach ($fileReadable as $file) {
-                    $filePath = str_replace('\\', '/', $file);
-                    $fileSum = md5_file($filePath);
+                    $fileReadable = $result['file_readable'];
+                    $fileNotWritable = $result['file_not_readable'];
+                    $fileReadable = sortByLastModified($fileReadable);
 
-                    if (in_array($fileSum, $whitelistMD5Sums)) { // if in whitelist skip
-                        continue;
-                    } elseif (in_array($fileSum, $blacklistMD5Sums)) { // if in blacklist alert and remove
-                        echo sprintf('<tr><td><span style="color:red;">%s (Blacklist)</span></td></tr>', $filePath);
-                        unlink($filePath);
-                        continue;
-                    } // else check the token
-            
-                    $tokens = getFileTokens($filePath);
-                    $cmp = compareTokens($tokenNeedles, $tokens);
-                    $cmp = implode(', ', $cmp);
+                    foreach ($fileReadable as $file) {
+                        $filePath = str_replace('\\', '/', $file);
+                        $fileSum = md5_file($filePath);
 
-                    if (!empty($cmp)) {
-                        echo sprintf('<tr><td><span style="color:orange;">%s (%s)</span></td></tr>', $filePath, $cmp);
+                        if (in_array($fileSum, $whitelistMD5Sums)) { // if in whitelist skip
+                            continue;
+                        } elseif (in_array($fileSum, $blacklistMD5Sums)) { // if in blacklist alert and remove
+                            echo sprintf('<tr><td><span style="color:red;">%s (Blacklist)</span></td></tr>', $filePath);
+                            unlink($filePath);
+                            continue;
+                        } // else check the token
+
+                        $tokens = getFileTokens($filePath);
+                        $cmp = compareTokens($tokenNeedles, $tokens);
+                        $cmp = implode(', ', $cmp);
+
+                        if (!empty($cmp)) {
+                            echo sprintf('<tr><td><span style="color:orange;">%s (%s)</span></td></tr>', $filePath, $cmp);
+                        }
                     }
                 }
-            }
+                ?>
+            </table>
+        </form>
+    </body>
+</html>
